54d2ca019ea3fce566dc088455ff23e84665090c
devwalex <salamiusmanolawale@gmail.com>
2020-04-23 17:27:33 +0100

    Header and Stock component

---
+++ src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="container">
+        <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+
+        <div class="collapse navbar-collapse">
+          <ul class="navbar-nav mr-auto">
+            <router-link
+              :to="{ name: 'Portfolio' }"
+              class="nav-item"
+              active-class="active"
+              tag="li"
+              ><a class="nav-link">Portfolio</a>
+            </router-link>
+            <router-link
+              :to="{ name: 'Stocks' }"
+              class="nav-item"
+              active-class="active"
+              tag="li"
+              ><a class="nav-link">Stocks</a>
+            </router-link>
+          </ul>
+          <ul class="navbar-nav navbar-right">
+            <li><a class="nav-link" href="#">End Day</a></li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Save & Load
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Save Data</a>
+                <a class="dropdown-item" href="#">Load Data</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
---
+++ src/components/Stock.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="col-sm-6 col-md-4">
+    <div class="card mb-3">
+      <div class="card-header text-white bg-primary">
+        {{ stock.name }} <small>(Price: {{ stock.price }})</small>
+      </div>
+      <div class="card-body">
+        <form class="form-inline">
+          <input
+            class="form-control"
+            type="number"
+            v-model="quantity"
+            placeholder="Quality"
+          />
+          <button
+            @click.prevent="buyStock"
+            class="btn btn-primary ml-3"
+            :disabled="quantity <= 0 || !Number.isInteger(parseInt(quantity))"
+          >
+            Buy
+          </button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['stock'],
+  data() {
+    return {
+      quantity: 0
+    }
+  },
+  methods: {
+    buyStock() {
+      console.log('BUY STOCK', typeof this.quantity)
+    }
+  }
+}
+</script>
+
+<style scoped></style>
