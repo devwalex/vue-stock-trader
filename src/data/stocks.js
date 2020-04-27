bae5c78720aa5350810ad339c30b14582983744f
devwalex <salamiusmanolawale@gmail.com>
2020-04-27 14:48:45 +0100

    add mock data for stocks

---
+++ src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+  { id: 1, name: 'BMW', price: '200' },
+  { id: 2, name: 'Apple', price: '100' },
+  { id: 3, name: 'Twitter', price: '40' },
+  { id: 4, name: 'Lexus', price: '300' }
+]
