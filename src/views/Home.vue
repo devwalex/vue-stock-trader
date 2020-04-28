c77e2e66cb2887c35102dcff3ed29b4f8d4d2319
devwalex <salamiusmanolawale@gmail.com>
2020-04-28 10:13:57 +0100

    add description and funds to the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,21 @@
 <template>
-  <div>
-    <h1>Home</h1>
+  <div class="mt-5">
+    <h1>Trade or View your Portfolio</h1>
+    <h6>You may Save and Load your Data</h6>
+    <h6>Click on <b>End Day</b> to begin a New Day!</h6>
+    <hr />
+    <p>Your Funds: {{ funds }}</p>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    funds() {
+      return this.$store.getters['portfolio/funds']
+    }
+  }
+}
 </script>
 
 <style scoped></style>
