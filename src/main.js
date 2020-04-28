2018686b7eeaf8519de90b7fdfc72c301cc30e73
devwalex <salamiusmanolawale@gmail.com>
2020-04-28 15:58:45 +0100

    add nprogress

--- src/main.js
+++ src/main.js
@@ -2,11 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 Vue.config.productionTip = false
 
 Vue.filter('currency', value => {
   return '£' + value.toLocaleString()
+})
+
+router.beforeEach((routeTo, routeFrom, next) => {
+  NProgress.start()
+  next()
+})
+router.afterEach(() => {
+  NProgress.done()
 })
 
 new Vue({
