523271a25459fc4653873009fac03abdea43a5ad
devwalex <salamiusmanolawale@gmail.com>
2020-04-30 10:20:29 +0100

    add hamburger icon in the menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,26 +3,35 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
       <div class="container">
         <router-link to="/" class="navbar-brand">Stock Trader</router-link>
-
-        <div class="collapse navbar-collapse">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <router-link
               :to="{ name: 'Portfolio' }"
               class="nav-item"
               active-class="active"
               tag="li"
-              ><a class="nav-link">Portfolio</a>
+            >
+              <a class="nav-link">Portfolio</a>
             </router-link>
-            <router-link
-              :to="{ name: 'Stocks' }"
-              class="nav-item"
-              active-class="active"
-              tag="li"
-              ><a class="nav-link">Stocks</a>
+            <router-link :to="{ name: 'Stocks' }" class="nav-item" active-class="active" tag="li">
+              <a class="nav-link">Stocks</a>
             </router-link>
           </ul>
           <ul class="navbar-nav navbar-right">
-            <li><a class="nav-link" href="#" @click="endDay">End Day</a></li>
+            <li>
+              <a class="nav-link" href="#" @click="endDay">End Day</a>
+            </li>
             <li class="nav-item dropdown">
               <a
                 class="nav-link dropdown-toggle"
@@ -32,16 +41,10 @@
                 data-toggle="dropdown"
                 aria-haspopup="true"
                 aria-expanded="false"
-              >
-                Save & Load
-              </a>
+              >Save & Load</a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#" @click="saveData"
-                  >Save Data</a
-                >
-                <a class="dropdown-item" href="#" @click="loadData"
-                  >Load Data</a
-                >
+                <a class="dropdown-item" href="#" @click="saveData">Save Data</a>
+                <a class="dropdown-item" href="#" @click="loadData">Load Data</a>
               </div>
             </li>
             <li class="m-auto text-white">
